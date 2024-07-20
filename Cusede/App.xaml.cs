0ec73fd73123e4ba972949531a4462f8f8e57ad6
Nico <Nico@DESKTOP-5MTEKNK>
2024-07-20 14:21:44 -0300

    Agregar archivos de proyecto.

---
+++ Cusede/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cusede
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
